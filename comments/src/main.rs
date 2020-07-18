eeb8a0e4625c040648aa371be6f4037fe04cab36
Ali GOREN <goren.ali@yandex.com>
2020-07-18 16:39:50 +0300

    Comments added

---
+++ comments/src/main.rs
@@ -0,0 +1,17 @@
+// There are two ways to write comments in Rust. This was the first way.
+
+/*
+This is second way to write comments.
+*/
+
+
+// Bu Rust recommending these kinds of comments. So, if you're starting comments with double slashes, it would be good.
+// Even if you're writing multiple lines of comments, this will be good.
+fn hello_world() {
+    // When your comments here, it will be good
+    println!("Hello world!"); // Comments can also be here
+}
+
+fn main() {
+    hello_world();
+}
